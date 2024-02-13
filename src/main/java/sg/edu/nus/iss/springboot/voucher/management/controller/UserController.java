dc9d7f838ce497b90356229d1d1c012e482fe1ac
Thet-Nandar-Aung <e1327955@u.nus.edu>
2024-02-13 12:24:47 +0800

    Add Test Property Source for DB connection.

--- src/main/java/sg/edu/nus/iss/springboot/voucher/management/controller/UserController.java
+++ src/main/java/sg/edu/nus/iss/springboot/voucher/management/controller/UserController.java
@@ -66,8 +66,8 @@ public class UserController {
 
 						if (isImageExists) {
 
-							String presignedUrl = GeneralUtility.makeNotNull(
-									imgUpload.generatePresignedUrl(s3Client, securityConfig, imageUrl));
+							String presignedUrl = GeneralUtility
+									.makeNotNull(imgUpload.generatePresignedUrl(s3Client, securityConfig, imageUrl));
 
 							userResp.setImage(presignedUrl);
 						}
@@ -92,10 +92,9 @@ public class UserController {
 
 	@PostMapping(value = "/create", produces = "application/json")
 	public ResponseEntity<UserResponse> createUser(@RequestPart("user") User user,
-			 @RequestPart(value = "image", required = false) MultipartFile uploadFile) {
+			@RequestPart(value = "image", required = false) MultipartFile uploadFile) {
 		logger.info("Call user create API...");
 
-		
 		String message = "";
 
 		ArrayList<ResultItem> resultList = new ArrayList<ResultItem>();
@@ -103,53 +102,61 @@ public class UserController {
 		UserResponse userResponse = new UserResponse();
 		ResultItem result = new ResultItem();
 		try {
+			if (!GeneralUtility.makeNotNull(user.getEmail()).equals("")) {
+				User dbUser = userService.findByEmail(user.getEmail());
+				String presignedUrl = "";
+				if (GeneralUtility.makeNotNull(dbUser).equals("")) {
 
-			User dbUser = userService.findByEmail(user.getEmail());
-			String presignedUrl = "";
-			if (GeneralUtility.makeNotNull(dbUser).equals("")) {
+					// if User not found, will create
 
-				// if User not found, will create
+					if (!GeneralUtility.makeNotNull(uploadFile).equals("")) {
+						logger.info("create user: " + user.getEmail() + "::" + uploadFile.getOriginalFilename());
+						ImageUploadToS3 imgUpload = new ImageUploadToS3();
+						presignedUrl = GeneralUtility.makeNotNull(
+								imgUpload.generatePresignedUrlAndUploadObject(s3Client, securityConfig, uploadFile));
+						if (!presignedUrl.equals("")) {
+							String imageUrl = securityConfig.getS3imagePrefix().trim() + "/"
+									+ uploadFile.getOriginalFilename().trim();
+							user.setImage(imageUrl);
+						}
 
-				if (!GeneralUtility.makeNotNull(uploadFile).equals("")) {
-					logger.info("create user: " + user.getEmail() + "::" + uploadFile.getOriginalFilename());
-					ImageUploadToS3 imgUpload = new ImageUploadToS3();
-					presignedUrl = GeneralUtility
-							.makeNotNull(imgUpload.generatePresignedUrlAndUploadObject(s3Client, securityConfig, uploadFile));
-					if (!presignedUrl.equals("")) {
-						String imageUrl = securityConfig.getS3imagePrefix().trim() + "/"
-								+ uploadFile.getOriginalFilename().trim();
-						user.setImage(imageUrl);
 					}
 
-				}
+					User createdUser = userService.create(user);
+					if (!GeneralUtility.makeNotNull(createdUser).equals("")) {
 
-				User createdUser = userService.create(user);
-				if (!GeneralUtility.makeNotNull(createdUser).equals("")) {
+						message = "User created successfully.";
+						logger.info(message + user.getEmail());
+						result.setEmail(createdUser.getEmail());
+						result.setUsername(createdUser.getUsername());
+						result.setRole(createdUser.getRole());
+						result.setImage(presignedUrl);
+						resultList.add(result);
+						userResponse.setMessage(message);
+						userResponse.setResult(resultList);
 
-					message = "User created successfully.";
-					logger.info(message + user.getEmail());
-					result.setEmail(createdUser.getEmail());
-					result.setUsername(createdUser.getUsername());
-					result.setRole(createdUser.getRole());
-					result.setImage(presignedUrl);
-					resultList.add(result);
-					userResponse.setMessage(message);
-					userResponse.setResult(resultList);
+						return new ResponseEntity<>(userResponse, HttpStatus.OK);
 
-					return new ResponseEntity<>(userResponse, HttpStatus.OK);
+					} else {
+						message = "Create user failed: Unable to create a new user account with email :"
+								+ user.getEmail();
+						logger.error(message);
+						resultList.add(result);
+						userResponse.setMessage(message);
+						userResponse.setResult(new ArrayList<ResultItem>());
+						return new ResponseEntity<>(userResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+					}
 
 				} else {
-					message = "Create user failed: Unable to create a new user account with email :" + user.getEmail();
+
+					message = "User already exists.";
 					logger.error(message);
-					resultList.add(result);
 					userResponse.setMessage(message);
-					userResponse.setResult(new ArrayList<ResultItem>());
-					return new ResponseEntity<>(userResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+					userResponse.setResult(resultList);
+					return new ResponseEntity<>(userResponse, HttpStatus.BAD_REQUEST);
 				}
-
 			} else {
-
-				message = "User already exists.";
+				message = "Bad Request.";
 				logger.error(message);
 				userResponse.setMessage(message);
 				userResponse.setResult(resultList);
@@ -172,7 +179,6 @@ public class UserController {
 
 		logger.info("Call user update API...");
 
-		
 		String message = "";
 
 		ArrayList<ResultItem> resultList = new ArrayList<ResultItem>();
@@ -180,65 +186,72 @@ public class UserController {
 		UserResponse userResponse = new UserResponse();
 		ResultItem result = new ResultItem();
 		try {
-			User dbUser = userService.findByEmail(user.getEmail());
-			String presignedUrl = "";
+			if (!GeneralUtility.makeNotNull(user.getEmail()).equals("")) {
+				User dbUser = userService.findByEmail(user.getEmail());
+				String presignedUrl = "";
 
-			if (!GeneralUtility.makeNotNull(dbUser).equals("")) {
+				if (!GeneralUtility.makeNotNull(dbUser).equals("")) {
 
-				if (!GeneralUtility.makeNotNull(uploadFile).equals("")) {
-					
-					logger.info("Update user : " + user.getEmail() + "::" + uploadFile.getOriginalFilename());
+					if (!GeneralUtility.makeNotNull(uploadFile).equals("")) {
 
-					
-					ImageUploadToS3 imgUpload = new ImageUploadToS3();
-					presignedUrl = GeneralUtility
-							.makeNotNull(imgUpload.generatePresignedUrlAndUploadObject(s3Client, securityConfig, uploadFile));
-					if (!presignedUrl.equals("")) {
-						String imageUrl = securityConfig.getS3imagePrefix().trim() + "/"
-								+ uploadFile.getOriginalFilename().trim();
-						user.setImage(imageUrl);
+						logger.info("Update user : " + user.getEmail() + "::" + uploadFile.getOriginalFilename());
+
+						ImageUploadToS3 imgUpload = new ImageUploadToS3();
+						presignedUrl = GeneralUtility.makeNotNull(
+								imgUpload.generatePresignedUrlAndUploadObject(s3Client, securityConfig, uploadFile));
+						if (!presignedUrl.equals("")) {
+							String imageUrl = securityConfig.getS3imagePrefix().trim() + "/"
+									+ uploadFile.getOriginalFilename().trim();
+							user.setImage(imageUrl);
+						}
+
 					}
 
-				}
+					dbUser.setUsername(user.getUsername());
+					dbUser.setPassword(user.getPassword());
+					dbUser.setRole(user.getRole());
+					dbUser.setActive(user.isActive());
+					dbUser.setImage(user.getImage());
 
-				dbUser.setUsername(user.getUsername());
-				dbUser.setPassword(user.getPassword());
-				dbUser.setRole(user.getRole());
-				dbUser.setActive(user.isActive());
-				dbUser.setImage(user.getImage());
+					User updatedUser = userService.update(dbUser);
+					if (!GeneralUtility.makeNotNull(updatedUser).equals("")) {
 
-				User updatedUser = userService.update(dbUser);
-				if (!GeneralUtility.makeNotNull(updatedUser).equals("")) {
+						message = "User updated successfully.";
+						logger.info(message + user.getEmail());
+						result.setEmail(updatedUser.getEmail());
+						result.setUsername(updatedUser.getUsername());
+						result.setRole(updatedUser.getRole());
+						result.setImage(presignedUrl);
+						resultList.add(result);
+						userResponse.setMessage(message);
+						userResponse.setResult(resultList);
+						return new ResponseEntity<>(userResponse, HttpStatus.OK);
+					} else {
+						message = "Update user failed: Changes could not be applied to the user with email :"
+								+ user.getEmail();
+						logger.error(message);
+						resultList.add(result);
+						userResponse.setMessage(message);
+						userResponse.setResult(new ArrayList<ResultItem>());
+						return new ResponseEntity<>(userResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+					}
 
-					message = "User updated successfully.";
-					logger.info(message + user.getEmail());
-					result.setEmail(updatedUser.getEmail());
-					result.setUsername(updatedUser.getUsername());
-					result.setRole(updatedUser.getRole());
-					result.setImage(presignedUrl);
-					resultList.add(result);
-					userResponse.setMessage(message);
-					userResponse.setResult(resultList);
-					return new ResponseEntity<>(userResponse, HttpStatus.OK);
 				} else {
-					message = "Update user failed: Changes could not be applied to the user with email :"
-							+ user.getEmail();
+
+					message = "User Not found.";
 					logger.error(message);
 					resultList.add(result);
 					userResponse.setMessage(message);
 					userResponse.setResult(new ArrayList<ResultItem>());
-					return new ResponseEntity<>(userResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+					return new ResponseEntity<>(userResponse, HttpStatus.NOT_FOUND);
+
 				}
-
 			} else {
-
-				message = "User Not found.";
+				message = "Bad Request.";
 				logger.error(message);
-				resultList.add(result);
 				userResponse.setMessage(message);
-				userResponse.setResult(new ArrayList<ResultItem>());
-				return new ResponseEntity<>(userResponse, HttpStatus.NOT_FOUND);
-
+				userResponse.setResult(resultList);
+				return new ResponseEntity<>(userResponse, HttpStatus.BAD_REQUEST);
 			}
 
 		} catch (Exception e) {
@@ -261,30 +274,38 @@ public class UserController {
 		String message = "";
 		try {
 
-			User dbUser = userService.findByEmailAndStatus(resetPwdReq.getEmail(), true);
-			if (!GeneralUtility.makeNotNull(dbUser).equals("")) {
+			if (!GeneralUtility.makeNotNull(resetPwdReq.getEmail()).equals("")) {
+				User dbUser = userService.findByEmailAndStatus(resetPwdReq.getEmail(), true);
+				if (!GeneralUtility.makeNotNull(dbUser).equals("")) {
 
-				dbUser.setPassword(resetPwdReq.getPassword());
-				User modifiedUser = userService.update(dbUser);
+					dbUser.setPassword(resetPwdReq.getPassword());
+					User modifiedUser = userService.update(dbUser);
 
-				if (!GeneralUtility.makeNotNull(modifiedUser).equals("")) {
-					message = "Reset Password Completed.";
-					logger.info(message + resetPwdReq.getEmail());
-					response.setMessage(message);
-					return new ResponseEntity<>(response, HttpStatus.OK);
+					if (!GeneralUtility.makeNotNull(modifiedUser).equals("")) {
+						message = "Reset Password Completed.";
+						logger.info(message + resetPwdReq.getEmail());
+						response.setMessage(message);
+						return new ResponseEntity<>(response, HttpStatus.OK);
+					} else {
+						message = "Reset Password failed: Unable to reset password for the user with email :"
+								+ resetPwdReq.getEmail();
+						logger.error(message);
+						response.setMessage(message);
+						return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+
+					}
 				} else {
-					message = "Reset Password failed: Unable to reset password for the user with email :"
-							+ resetPwdReq.getEmail();
+					message = "Reset Password failed: Unable to find the user with email :" + resetPwdReq.getEmail();
 					logger.error(message);
 					response.setMessage(message);
-					return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-
+					return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
 				}
 			} else {
-				message = "Reset Password failed: Unable to find the user with email :" + resetPwdReq.getEmail();
+				message = "Bad Request.";
 				logger.error(message);
 				response.setMessage(message);
-				return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+				return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+
 			}
 		} catch (Exception e) {
 			logger.error("Error, " + e.toString());
--- src/test/java/sg/edu/nus/iss/springboot/voucher/management/service/UserControllerTest.java
+++ src/test/java/sg/edu/nus/iss/springboot/voucher/management/service/UserControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -24,6 +25,10 @@ import sg.edu.nus.iss.springboot.voucher.management.service.impl.UserService;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource(properties = {
+        "DB_USERNAME=admin",
+        "DB_PASSWORD=RDS_12345"
+})
 public class UserControllerTest {
 
 	@Autowired
